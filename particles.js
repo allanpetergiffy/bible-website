46aaa9b47b6d65440cbff7e2f9d2ac00c4c914c2
allanpetergiffy <wallanpetergiffy@gmail.com>
2026-02-27 00:26:01 +0530

    Add particle system implementation in particles.js

---
+++ particles.js
@@ -0,0 +1,41 @@
+const canvas = document.getElementById("particles");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray = [];
+
+class Particle {
+  constructor(x, y, size, color, speedX, speedY) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.color = color;
+    this.speedX = speedX;
+    this.speedY = speedY;
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (this.size > 0.2) this.size -= 0.01;
+  }
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+function initParticles() {
+  particlesArray = [];
+  for (let i = 0; i < 120; i++) {
+    let size = Math.random() * 3 + 1;
+    let x = Math.random() * canvas.width;
+    let y = Math.random() * canvas.height;
+    let speedX = (Math.random() - 0.5) * 0.3;
+    let speedY = (Math.random() - 0.5) * 0.3;
+    let color = `hsla(${Math.random() * 360}, 70%, 60%, 0.8)`;
+    particlesArray.push(new Particle(x, y, size, color, speedX, speedY));
+  }
